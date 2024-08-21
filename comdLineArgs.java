8435f9dbac2136b9328e6cffdc5ebdae39f2fed2
ardeep01 <ardeepkaur01@gmail.com>
2024-08-21 10:14:17 +0530

    Added new file

---
+++ comdLineArgs.java
@@ -0,0 +1,11 @@
+package myJava;
+
+public class comdLineArgs {
+    public static void main(String[] args) {
+        for (int i=0; i<args.length; i++){
+            System.out.println("Argument:" + i + "value:"+ args[i] );
+        }
+    }
+}
+    
+
