b4d3f608a5ca5e1be6b88e49a279622298967da3
ardeep01 <ardeepkaur01@gmail.com>
2024-08-21 10:04:43 +0530

    String Concatination

--- helloWorld.java
+++ helloWorld.java
@@ -1,3 +1,4 @@
+package myJava;
 public class helloWorld{
     public static void main(String args[]){
         System.out.print("Hello World!");
---
+++ stringCon.java
@@ -0,0 +1,10 @@
+package myJava;
+
+public class stringCon {
+    public static void main(String args[]){
+        int i=30;
+        int j=34;
+        System.out.println("Hellow"+i);
+        System.out.println(i+j);
+    }
+}
